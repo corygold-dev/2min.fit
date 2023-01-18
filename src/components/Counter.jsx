b919c7c1d1db23d61c604087ffe282e9592cad76
William Gold <goldcorum@gmail.com>
2023-01-18 17:16:35 -0500

    Finish counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,9 @@
 import { useEffect, useState } from 'react';
 
 function Counter() {
-  const [counter, setCounter] = useState(5);
+  const [counter, setCounter] = useState(30);
   const [timerStarted, setTimerStarted] = useState(false);
-  const [heading, setHeading] = useState('Time Remaining');
   const [buttonContent, setButtonContent] = useState('Start');
-
-  console.log(counter);
 
   function handleButton() {
     if (buttonContent === 'Start') {
@@ -16,12 +13,17 @@ function Counter() {
       setTimerStarted(false);
       setButtonContent('Reset');
     } else {
-      setCounter(5);
+      setTimerStarted(false);
+      setCounter(30);
       setButtonContent('Start');
     }
   }
 
   useEffect(() => {
+    function handleButtonContent() {
+      if (counter < 1) setButtonContent('Reset');
+    }
+    handleButtonContent();
     timerStarted &&
       counter > 0 &&
       setTimeout(() => setCounter(counter - 1), 1000);
@@ -29,7 +31,6 @@ function Counter() {
 
   return (
     <div>
-      <div>{heading}</div>
       <div>{counter}</div>
       <button onClick={handleButton}>{buttonContent}</button>
     </div>
