fd85a5fdd344d9c194ef5fb6ab82cd0738a1be88
Cory <goldcorum@gmail.com>
2023-02-17 12:37:12 -0500

    Create exercises file

---
+++ src/exercises.js
@@ -0,0 +1,29 @@
+const exercises = {
+  1: {
+    upper: "push-up",
+    lower: "air squat",
+    stretch: "right shoulder stretch",
+  },
+  2: {
+    upper: "pull-up",
+    lower: "glute bridge",
+    stretch: "squat prying",
+  },
+  3: {
+    upper: "chair dip",
+    lower: "lunge",
+    stretch: "sitting V stretch",
+  },
+  4: {
+    upper: "row",
+    lower: "good morning",
+    stretch: "lying pigeon",
+  },
+  5: {
+    upper: "8 count pushup",
+    lower: "8 count pushup",
+    strech: "hip flexor",
+  },
+};
+
+export default exercises;
