a8fe4683e568e2b9bbfa7639492ae56da3f8ddb2
corumgold <goldcorum@gmail.com>
2023-03-01 17:26:16 -0500

    Add instructions

--- src/components/Instructions/Instructions.jsx
+++ src/components/Instructions/Instructions.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Instructions.css";
 
 export default function Instructions() {
+  const [instructionsShown, setInstructionsShown] = useState(true);
+
   return (
-    <div id="instructions">
-      <div id="top-instructions">
-        <p>Click on the Exercise name for an instructional video</p>
-      </div>
-      <div id="bottom-instructions">
+    <div id={instructionsShown ? "instructions" : "hiddenInstructions"}>
+      <div>
         <p>
-          Use the start button to start your timer, a 3 second "Get Ready"
+          Click the exercise name for a how-to video. The "Next Exercise" and
+          "Previous Exercise" buttons navigate through your daily three
+          exercises (two strength exercises and one stretching).
+        </p>
+      </div>
+      <div>
+        <p>
+          Use the start button to start your timer - a three second "Get Ready"
           countdown allows you to get into position for your exercise!
         </p>
+        <button onClick={() => setInstructionsShown(false)}>Close</button>
       </div>
     </div>
   );
