4cfacf179c9761cf7489c56dda73ee332e199cd8
corumgold <goldcorum@gmail.com>
2023-03-01 14:21:29 -0500

    Finish filling in exercises data

--- src/exercises.js
+++ src/exercises.js
@@ -6,36 +6,55 @@ const exercises = {
     },
     2: {
       exerciseName: "Air Squat",
-      link: "link",
+      link: "https://www.youtube.com/watch?v=NGttLyYktTw&ab_channel=cultfitOfficial",
     },
     3: {
       exerciseName: "Shoulder Stretch",
-      link: "link",
+      link: "https://www.youtube.com/watch?v=bN6v5HcHIEw&ab_channel=LIVESTRONG.COM",
     },
   },
   2: {
     1: {
       exerciseName: "Pull Up",
-      link: "link",
+      link: "https://www.youtube.com/watch?v=eGo4IYlbE5g&ab_channel=Calisthenicmovement",
     },
     2: {
-      exerciseName: "Glute Bridge",
-      link: "link",
+      exerciseName: "Floor",
+      link: "https://www.youtube.com/watch?v=NqZ6JUIx0I8&ab_channel=Tangelo-SeattleChiropractor%2BRehab",
     },
     3: {
-      exerciseName: "Prying Goblet Squat",
-      link: "link",
+      exerciseName: "Prying Squat",
+      link: "https://www.youtube.com/watch?v=QSJKFkOu59g&ab_channel=PowerCompanyClimbing",
     },
   },
   3: {
-    1: { exerciseName: "Chair Dip", link: "link" },
-    2: { exerciseName: "Lunge", link: "link" },
-    3: { exerciseName: "Pancake Stretch", link: "link" },
+    1: {
+      exerciseName: "Chair Dip",
+      link: "https://www.youtube.com/watch?v=u5HbUxh40Y4&ab_channel=BodyFlyFitness",
+    },
+    2: {
+      exerciseName: "Lunge",
+      link: "https://www.youtube.com/watch?v=qeJ3Si5r5y0&ab_channel=GirlsGoneStrongz  ",
+    },
+    3: {
+      exerciseName: "Butterfly Stretch",
+      link: "https://www.youtube.com/watch?v=MdE_Cj6ChLo&ab_channel=COREChiropractic",
+    },
   },
+
   4: {
-    1: { exerciseName: "Bodyweight Row", link: "link" },
-    2: { exerciseName: "Good Morning", link: "link" },
-    3: { exerciseName: "Lying Pigeon", link: "link" },
+    1: {
+      exerciseName: "Bodyweight Row",
+      link: "https://www.youtube.com/watch?v=rloXYB8M3vU&ab_channel=AntranikDotOrg",
+    },
+    2: {
+      exerciseName: "Good Morning",
+      link: "https://www.youtube.com/watch?v=nczH_7m1TnI&ab_channel=CrossfitCDA",
+    },
+    3: {
+      exerciseName: "Lying Pigeon",
+      link: "https://www.youtube.com/watch?v=op-eDU9eNqM&ab_channel=AskDoctorJo",
+    },
   },
   5: {
     1: {
