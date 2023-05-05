ac0cf4d3cb0481c2b32f0e447c06f51a5f05d55c
Cory Gold <goldcorum@gmail.com>
2023-05-05 12:25:21 -0400

    Change variables in exercise display for readability

--- src/components/ExerciseDisplay/ExerciseDisplay.jsx
+++ src/components/ExerciseDisplay/ExerciseDisplay.jsx
@@ -13,14 +13,13 @@ export default function ExerciseDisplay() {
     exerciseBlock = program[Math.floor(Math.random() * (5 - 1 + 1) + 1)];
   }
 
+  const exerciseName = exerciseBlock[exerciseNum]["exerciseName"];
+  const exerciseLink = exerciseBlock[exerciseNum]["link"];
+
   return (
     <div id="exercise-display">
-      <a
-        href={exerciseBlock[exerciseNum]["link"]}
-        target="_blank"
-        rel="noreferrer"
-      >
-        {exerciseBlock[exerciseNum]["exerciseName"]}
+      <a href={exerciseLink} target="_blank" rel="noreferrer">
+        {exerciseName} ({exerciseNum}) 
       </a>
     </div>
   );
