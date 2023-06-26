b25979bdec793197fc246edccc83572365289c9c
Cory Gold <goldcorum@gmail.com>
2023-06-26 11:34:09 -0400

    Add a random button to randomize exercises for the user

--- src/components/ProgramSelector/ProgramSelector.jsx
+++ src/components/ProgramSelector/ProgramSelector.jsx
@@ -7,37 +7,48 @@ import { expert } from "../../exercises/expert";
 // import { atgExercises } from "../../exercises/atg";
 
 export default function ProgramSelector() {
-  const { setProgram, program } = useContext(ProgramContext);
+  const { setProgram, program, setRandom, random } = useContext(ProgramContext);
 
   return (
-    <div id="program-buttons">
-      <button
-        id="beginner-button"
-        className={program === beginner ? "selected" : "not-selected"}
-        onClick={() => setProgram(beginner)}
-      >
-        Beginner
-      </button>
-      <button
-        id="intermediate-button"
-        className={program === intermediate ? "selected" : "not-selected"}
-        onClick={() => setProgram(intermediate)}
-      >
-        Intermediate
-      </button>
-      <button
-        id="expert-button"
-        className={program === expert ? "selected" : "not-selected"}
-        onClick={() => setProgram(expert)}
-      >
-        Expert
-      </button>
-      {/* <button
+    <>
+      <div>
+        <button
+          id="random-button"
+          className={random ? "selected" : "not-selected"}
+          onClick={() => setRandom(!random)}
+        >
+          Random
+        </button>
+      </div>
+      <div id="program-buttons">
+        <button
+          id="beginner-button"
+          className={program === beginner ? "selected" : "not-selected"}
+          onClick={() => setProgram(beginner)}
+        >
+          Beginner
+        </button>
+        <button
+          id="intermediate-button"
+          className={program === intermediate ? "selected" : "not-selected"}
+          onClick={() => setProgram(intermediate)}
+        >
+          Intermediate
+        </button>
+        <button
+          id="expert-button"
+          className={program === expert ? "selected" : "not-selected"}
+          onClick={() => setProgram(expert)}
+        >
+          Expert
+        </button>
+        {/* <button
         className={program === atgExercises ? "selected" : null}
         onClick={() => setProgram(atgExercises)}
       >
         ATG
       </button> */}
-    </div>
+      </div>
+    </>
   );
 }
--- src/providers/ProgramProvider.js
+++ src/providers/ProgramProvider.js
@@ -5,16 +5,18 @@ const ProgramContext = createContext();
 
 function ProgramProvider({ children }) {
   const [program, setProgram] = useState(intermediate);
+  const [random, setRandom] = useState(false);
+
   const day = new Date().getDay();
   let exerciseBlock = program[day];
 
-  if (day === 0 || day === 6) {
+  if (day === 0 || day === 6 || random) {
     exerciseBlock = program[Math.floor(Math.random() * (5 - 1 + 1) + 1)];
   }
 
   return (
     <ProgramContext.Provider
-      value={{ program, setProgram, exerciseBlock }}
+      value={{ program, setProgram, exerciseBlock, setRandom, random }}
     >
       {children}
     </ProgramContext.Provider>
